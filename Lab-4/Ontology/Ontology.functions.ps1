5b738c6a87b01db8cf0573d2008a839968454557
sethvs <sethvs@users.noreply.github.com>
2023-10-10 18:06:37 +0300

    Cleanup

--- Lab-4/Ontology/Ontology.functions.ps1
+++ Lab-4/Ontology/Ontology.functions.ps1
@@ -101,30 +101,7 @@ function Remove-OwlClass
     {
         $xml = New-Object -TypeName System.Xml.XmlDocument
         $xml.Load($path)
-<#
-        if ($ClassName -in $xml.Ontology.Declaration.Class.IRI.Trim('#'))
-        {
-            Write-Output -InputObject "There is already"
-        }
-        else
-        {
-            # $declaration = $xml.CreateNode([System.Xml.XmlNodeType]::Element, "owl", "Declaration", "http://www.w3.org/2002/07/owl#")
-            # $declaration = $xml.CreateNode([System.Xml.XmlNodeType]::Element, "owl", "Declaration", $null)
-            # $declaration = $xml.CreateNode([System.Xml.XmlNodeType]::Element, "owl", "Declaration", $xml.DocumentElement.NamespaceURI)
-            $declaration = $xml.CreateNode([System.Xml.XmlNodeType]::Element, "Declaration", $xml.DocumentElement.NamespaceURI)
-            
-            # $class = $xml.CreateNode([System.Xml.XmlNodeType]::Element, "owl", "Class", $null)
-            $class = $xml.CreateNode([System.Xml.XmlNodeType]::Element, "Class", $xml.DocumentElement.NamespaceURI)
 
-            $class.SetAttribute("IRI", "#" + $ClassName)
-
-            # $class.AppendChild($iri)
-            $declaration.AppendChild($class) | Out-Null
-            $xml.Ontology.AppendChild($declaration) | Out-Null
-
-            $xml.Save($path)
-        }
-#>
         if ($ClassName -in $xml.Ontology.Declaration.Class.IRI.Trim('#'))
         {
             if ($ClassName -in $xml.Ontology.ClassAssertion.Class.IRI.Trim('#') -or
